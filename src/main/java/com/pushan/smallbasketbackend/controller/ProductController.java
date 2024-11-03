e7d743d0affede06fbe5ed154a19e879b6bd88ff
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2024-11-03 16:05:24 +0530

    Added Put and Delete mappings

--- src/main/java/com/pushan/smallbasketbackend/controller/ProductController.java
+++ src/main/java/com/pushan/smallbasketbackend/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.pushan.smallbasketbackend.controller;
 import com.pushan.smallbasketbackend.model.Product;
 import com.pushan.smallbasketbackend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,13 +13,28 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return service.getProducts();
     }
 
-    @RequestMapping("/products/{id}")
+    @GetMapping("/products/{id}")
     public Product getProductById(@PathVariable int id) {
         return service.getProductById(id);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product) {
+        service.addProduct(product);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod) {
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable int id) {
+        service.deleteProduct(id);
     }
 }
--- src/main/java/com/pushan/smallbasketbackend/service/ProductService.java
+++ src/main/java/com/pushan/smallbasketbackend/service/ProductService.java
@@ -3,17 +3,18 @@ package com.pushan.smallbasketbackend.service;
 import com.pushan.smallbasketbackend.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "iPhone", 50000),
             new Product(102, "Canon Camera", 70000),
             new Product(103, "Shure Mic", 10000)
-    );
+    ));
 
     public List<Product> getProducts() {
         return products;
@@ -23,5 +24,27 @@ public class ProductService {
         return products.stream()
                 .filter(p -> p.getProdId() == id)
                 .findFirst().get();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void updateProduct(Product prod) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == prod.getProdId())
+                index = i;
+        }
+        products.set(index, prod);
+    }
+
+    public void deleteProduct(int id) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == id)
+                index = i;
+        }
+        products.remove(index);
     }
 }
