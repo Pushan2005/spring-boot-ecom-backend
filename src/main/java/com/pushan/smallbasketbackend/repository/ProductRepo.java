75cf12435683dc99bca84d7dff31a58cdd400426
Pushan Thimmegowda <pushan.sahanaganesh@gmail.com>
2024-11-03 16:50:51 +0530

    Replaced static product list with in-memory H2 database

---
+++ src/main/java/com/pushan/smallbasketbackend/repository/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.pushan.smallbasketbackend.repository;
+
+import com.pushan.smallbasketbackend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
--- src/main/java/com/pushan/smallbasketbackend/service/ProductService.java
+++ src/main/java/com/pushan/smallbasketbackend/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.pushan.smallbasketbackend.service;
 
 import com.pushan.smallbasketbackend.model.Product;
+import com.pushan.smallbasketbackend.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,41 +12,26 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(101, "iPhone", 50000),
-            new Product(102, "Canon Camera", 70000),
-            new Product(103, "Shure Mic", 10000)
-    ));
+    @Autowired
+    ProductRepo repo;
 
     public List<Product> getProducts() {
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int id) {
-        return products.stream()
-                .filter(p -> p.getProdId() == id)
-                .findFirst().get();
+        return repo.findById(id).orElse(new Product());
     }
 
     public void addProduct(Product product) {
-        products.add(product);
+        repo.save(product);
     }
 
     public void updateProduct(Product prod) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProdId() == prod.getProdId())
-                index = i;
-        }
-        products.set(index, prod);
+        repo.save(prod);
     }
 
     public void deleteProduct(int id) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProdId() == id)
-                index = i;
-        }
-        products.remove(index);
+        repo.deleteById(id);
     }
 }
